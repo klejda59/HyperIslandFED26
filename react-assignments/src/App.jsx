d2cb79801e466ece81b975f14af8ace2e581a6f7
klejda59 <klejda.mamaj@hyperisland.se>
2025-01-21 09:37:37 +0100

    a9

--- react-assignments/src/App.jsx
+++ react-assignments/src/App.jsx
@@ -1,24 +1,16 @@
 import "./App.css";
-import ChildComponent from "./Components/ChildComponent";
+import MyStateComponent from "./Components/MyStateComponent/MyStateComponent";
 
 function App() {
-  const person = {
-    name: "John",
-    info: {
-      age: 25,
-      city: "New York",
-      job: "Developer",
-      hobbies: ["reading", "coding", "gaming"],
-    },
-  };
 
   return (
-    <div>
+    <div className="container">
       <header>
-        <h1>This is React</h1>
+        <h1>Hello </h1>
       </header>
-      {/* TODO create a child component in proper jsx, the component takes the person object as props and render the data */}
-      <ChildComponent person={person} />
+      <main>
+      <MyStateComponent/>
+      </main>
     </div>
   );
 }
