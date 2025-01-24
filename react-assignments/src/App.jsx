9931cda70acd046addbec89000030eaedb4ac41a
klejda59 <klejda.mamaj@hyperisland.se>
2025-01-24 10:53:09 +0100

    router

--- react-assignments/src/App.jsx
+++ react-assignments/src/App.jsx
@@ -1,17 +1,33 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./Components/Nav/Nav";
+import Homepage from "./Components/Homepage/Homepage";
+import EffectsComponent from "./Components/Effect/EffectsComponent";
 import MyStateComponent from "./Components/MyStateComponent/MyStateComponent";
+import PropsComponent from "./Components/MyStateComponent/PropsComponent";
 
 function App() {
+  const person = {
+    name: "John",
+    info: {
+      age: 25,
+      city: "New York",
+      job: "Developer",
+      hobbies: ["reading", "coding", "gaming"],
+    },
+  };
 
+  //  TODO add routes to the components we've created so far
   return (
-    <div className="container">
-      <header>
-        <h1>Hello </h1>
-      </header>
-      <main>
-      <MyStateComponent/>
-      </main>
-    </div>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/props" element={<PropsComponent person={person} />} />
+        <Route path="/state" element={<MyStateComponent />} />
+        <Route path="/effects" element={<EffectsComponent />} />
+      </Routes>
+    </Router>
   );
 }
 
